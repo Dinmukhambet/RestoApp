6c68f608785c82700b9f4d1a4b5d701bc30aaea5
Yellow <dimashdim@gmail.com>
2019-05-12 13:45:19 +0600

    Initial commit

---
+++ src/main/java/kz/devyellow/RestoApp/RestoAppApplication.java
@@ -0,0 +1,13 @@
+package kz.devyellow.RestoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestoAppApplication.class, args);
+	}
+
+}
